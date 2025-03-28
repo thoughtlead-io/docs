d8af9a9cc6fade664d60d0d03ae3a069beb5280f
Torrey Leonard <torreydavidleonard@gmail.com>
2025-03-28 00:27:49 -0400

    update endpoints

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-npx @mintlify/scraping@latest openapi-file api/openapi.json -o api-reference -y
+npx @mintlify/scraping@latest openapi-file api-reference/openapi.json -o api-reference -y
 mintlify dev
